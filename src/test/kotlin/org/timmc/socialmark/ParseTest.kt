4cd39cc4ddefa761dafd3f03e4b9bd50bd613b5b
Tim McCormack <cortex@brainonfire.net>
2022-08-28 15:12:41 -0400

    Test outputs

--- src/test/kotlin/org/timmc/socialmark/ParseTest.kt
+++ src/test/kotlin/org/timmc/socialmark/ParseTest.kt
@@ -1,15 +1,24 @@
 package org.timmc.socialmark
 
 import kotlin.test.Test
+import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
 class ParseTest {
     @Test fun empty() {
-        Parse.parseMarkup("")
+        assertEquals(0, Parse.parseMarkup("").nodes.size)
     }
 
     @Test fun justText() {
-        println(Parse.parseMarkup("Hello, world!"))
+        mapOf(
+            "simple" to Parse.parseMarkup("Hello, world!"),
+            "escape" to Parse.parseMarkup("Hello,\\u20;world!"),
+        ).map { (which, doc) ->
+            assertEquals(
+                Document(listOf(Node.TextNode("Hello, world!"))), doc,
+                "Assertion failed for '$which'"
+            )
+        }
     }
 
 //        Parse.parseMarkup("<strong>Hello, world!</strong>")
